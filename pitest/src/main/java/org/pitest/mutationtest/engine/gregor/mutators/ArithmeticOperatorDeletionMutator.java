ccc5717ecb93a96e7c692adf599ce6381e17b50b
Adrian Rivera-Zayas <adrianriveraza@gmail.com>
2018-10-06 13:37:34 -0500

    Add three mutator files and complete AOR operator

---
+++ pitest/src/main/java/org/pitest/mutationtest/engine/gregor/mutators/ArithmeticOperatorDeletionMutator.java
@@ -0,0 +1,73 @@
+/*
+ * Copyright 2010 Henry Coles
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and limitations under the License.
+ */
+/*
+package org.pitest.mutationtest.engine.gregor.mutators;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.objectweb.asm.MethodVisitor;
+import org.objectweb.asm.Opcodes;
+import org.pitest.mutationtest.engine.gregor.AbstractInsnMutator;
+import org.pitest.mutationtest.engine.gregor.InsnSubstitution;
+import org.pitest.mutationtest.engine.gregor.MethodInfo;
+import org.pitest.mutationtest.engine.gregor.MethodMutatorFactory;
+import org.pitest.mutationtest.engine.gregor.MutationContext;
+import org.pitest.mutationtest.engine.gregor.ZeroOperandMutation;
+
+public enum ArithmeticOperatorDeletionMutator implements MethodMutatorFactory {
+
+  MATH_MUTATOR;
+
+  @Override
+  public MethodVisitor create(final MutationContext context,
+      final MethodInfo methodInfo, final MethodVisitor methodVisitor) {
+    return new MathMethodVisitor(this, methodInfo, context, methodVisitor);
+  }
+
+  @Override
+  public String getGloballyUniqueId() {
+    return this.getClass().getName();
+  }
+
+  @Override
+  public String getName() {
+    return name();
+  }
+
+}
+
+class MathMethodVisitor extends AbstractInsnMutator {
+
+  MathMethodVisitor(final MethodMutatorFactory factory,
+      final MethodInfo methodInfo, final MutationContext context,
+      final MethodVisitor writer) {
+    super(factory, methodInfo, context, writer);
+  }
+
+  private static final Map<Integer, ZeroOperandMutation> MUTATIONS = new HashMap<>();
+
+  static {
+
+
+  }
+
+  @Override
+  protected Map<Integer, ZeroOperandMutation> getMutations() {
+    return MUTATIONS;
+  }
+
+}
+ */
---
+++ pitest/src/main/java/org/pitest/mutationtest/engine/gregor/mutators/RelationalOperatorReplacementMutator.java
@@ -0,0 +1,55 @@
+/* package org.pitest.mutationtest.engine.gregor.mutators;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.objectweb.asm.MethodVisitor;
+import org.objectweb.asm.Opcodes;
+import org.pitest.mutationtest.engine.gregor.AbstractInsnMutator;
+import org.pitest.mutationtest.engine.gregor.InsnSubstitution;
+import org.pitest.mutationtest.engine.gregor.MethodInfo;
+import org.pitest.mutationtest.engine.gregor.MethodMutatorFactory;
+import org.pitest.mutationtest.engine.gregor.MutationContext;
+import org.pitest.mutationtest.engine.gregor.ZeroOperandMutation;
+
+public enum RelationalOperatorReplacementMutator implements MethodMutatorFactory {
+
+    MATH_MUTATOR;
+
+    @Override
+    public MethodVisitor create(final MutationContext context, final MethodInfo methodInfo,
+            final MethodVisitor methodVisitor) {
+        return new MathMethodVisitor(this, methodInfo, context, methodVisitor);
+    }
+
+    @Override
+    public String getGloballyUniqueId() {
+        return this.getClass().getName();
+    }
+
+    @Override
+    public String getName() {
+        return name();
+    }
+
+}
+
+class MathMethodVisitor extends AbstractInsnMutator {
+
+    MathMethodVisitor(final MethodMutatorFactory factory, final MethodInfo methodInfo, final MutationContext context,
+            final MethodVisitor writer) {
+        super(factory, methodInfo, context, writer);
+    }
+
+    private static final Map<Integer, ZeroOperandMutation> MUTATIONS = new HashMap<>();
+
+    static {
+
+    }
+
+    @Override
+    protected Map<Integer, ZeroOperandMutation> getMutations() {
+        return MUTATIONS;
+    }
+
+} */
